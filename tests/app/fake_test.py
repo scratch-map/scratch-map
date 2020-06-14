807c250bf6ebc856cbf99bb56295a4eb2876d936
Darrell Cox <darrellcox@hotmail.co.uk>
2020-06-14 20:37:36 +0100

    Added false test

--- tests/app/fake_test.py
+++ tests/app/fake_test.py
@@ -4,3 +4,6 @@ import unittest
 class TestInfo(unittest.TestCase):
     def fake_test(self):
         self.assertEqual(True, False)
+
+    def fake_test2(self):
+        self.assertEqual(True, True)
