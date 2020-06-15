f8ae4073bfb8c57e207a5b452eb82bcc64753cf3
Darrell Cox <darrellcox@hotmail.co.uk>
2020-06-15 21:19:33 +0100

    Travis update

---
+++ tests/app/test_fake_test.py
@@ -0,0 +1,9 @@
+import unittest
+
+
+class TestFake(unittest.TestCase):
+    def test_fake_test(self):
+        self.assertEqual(True, False)
+
+    def test_fake_test2(self):
+        self.assertEqual(True, True)
