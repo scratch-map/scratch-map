eba1d8c961cdd97db1925b118fea349960f44fca
tom reilly <treilly94@live.com>
2020-06-20 18:24:59 +0100

    Added unit tests for input

--- tests/test_init.py
+++ tests/test_init.py
@@ -16,6 +16,19 @@ class TestRoot(unittest.TestCase):
         # Input box
         self.assertIn(b'<input name="add_country">', response.data)
 
+    def test_input(self):
+        # Check first value is added
+        response1 = self.app.post("/", data={"add_country": "Wales"})
+        self.assertEqual(response1.status_code, 200)
+        self.assertIn(b"<p>Wales</p>", response1.data)
+        self.assertNotIn(b"<p>But I prefer England</p>", response1.data)
+
+        # Check second value is added and the first is still there
+        response2 = self.app.post("/", data={"add_country": "But I prefer England"})
+        self.assertEqual(response2.status_code, 200)
+        self.assertIn(b"<p>Wales</p>", response2.data)
+        self.assertIn(b"<p>But I prefer England</p>", response2.data)
+
 
 if __name__ == "__main__":
     unittest.main()
