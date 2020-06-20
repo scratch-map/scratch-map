75af66dd4bb7626749b9cbbda0c6171acd6fcc7c
tom reilly <treilly94@live.com>
2020-06-20 12:21:14 +0100

    added countries class

--- scratch_map/__init__.py
+++ scratch_map/__init__.py
@@ -2,6 +2,7 @@ from flask import render_template, Flask, request
 from structlog import get_logger
 
 from config import Config
+from scratch_map.countries import Countries
 
 logger = get_logger()
 
@@ -9,13 +10,13 @@ app = Flask(__name__)
 app.config.from_object(Config)
 app.strict_slashes = False
 
-countries = []
+countries = Countries()
 
 
 @app.route("/", methods=["GET", "POST"])
 def root():
     if request.method == "POST":
-        countries.append(request.form["add_country"])
+        countries.add_country(request.form["add_country"])
 
     return render_template("index.html")
 
